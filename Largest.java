2abda16057a8bb69b5b1c6cfa5ebf6de2f010f6b
chanduramineni <41728861+chanduramineni@users.noreply.github.com>
2018-08-01 13:56:10 +0530

    Create Largest.java

---
+++ Largest.java
@@ -0,0 +1,26 @@
+import java.util.*
+import java.lang.*
+import java.io.*
+
+
+class Largest
+ {
+          public static void main(String[] args)throws java.long.Exception
+          {
+          Scanner s=new Scanner(System.in);
+		int x=s.nextInt();
+		int y=s.nextInt();
+		int z=s.nextInt();
+		if(x>y)
+		{
+			System.out.println(x);
+		}
+		else if(y>z)
+		{
+			System.out.println(y);
+		}
+		else
+		System.out.println(z);
+		
+	}
+}
